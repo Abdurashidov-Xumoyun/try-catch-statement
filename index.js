2f7d80b1648fcb150fb5180ee26c85194c50a0d5
Abdurashidov-Xumoyun <okxumoyun@gmail.com>
2025-02-11 21:14:33 +0500

    try catch statement

---
+++ index.js
@@ -0,0 +1,28 @@
+let ellInputValue = document.querySelector(".inputText"),
+  ellBtn = document.querySelector(".btn"),
+  ellError = document.querySelector("span");
+
+function numFilter() {
+  let value = Number(ellInputValue.value)
+  ellError.style.color = "red";
+  try {
+    if (value === 0) {
+      throw "biron son kritishingiz mumkin"
+    }
+    if (value < 5) {
+      throw "5 dan kichik son kritdingiz";
+    } 
+    if (value > 10) {
+      throw "10 dan katta son kritdingiz";
+    }
+    if (!Number(value)) {
+      throw "son kritmadingiz";
+    }
+    if (value > 5 || value < 10) {
+      ellError.style.color = "green";
+      throw "to'g'ri son kritdingiz";
+    }
+  } catch (error) {
+    ellError.innerHTML = `Siz ${error}`
+  }
+}
